44362a408d6faef7a90ed8b1e70e0230b3267243
David Kelly <davek032@outlook.com>
2019-11-15 23:08:25 +0000

    Camera
    Started work on camera

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    //Player's head
+    public GameObject targetPoint;
+
+    void Update() 
+    {
+        camMove();
+
+    }
+
+    void camMove() 
+    {
+        float camXInput = Input.GetAxis("CamX");
+        print(camXInput);
+        float camYInput = Input.GetAxis("CamY");
+        print(camYInput);
+
+        if (camXInput <= -0.2)
+        {
+            transform.RotateAround(targetPoint.transform.position, transform.up*-1f, 1f);
+        }
+        //if (camXInput >= 0.2)
+        //{
+        //    transform.RotateAround(targetPoint.transform.position, transform.up, -1f);
+        //}
+
+    }
+}

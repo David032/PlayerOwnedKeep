34fe991fe81b662f4146317722669cdd0513d610
David Kelly <davek032@outlook.com>
2019-11-15 19:20:43 +0000

    Metric Tons
    Basic player set-up with click to move controls
    U.I set up with a few placeholder elements

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class PlayerController : MonoBehaviour
+{
+    NavMeshAgent agent;
+    Animator anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        anim = GetComponent<Animator>();
+
+        anim.SetInteger("WeaponType_int", 0);
+        anim.SetInteger("MeleeType_int", 0);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit;
+
+            if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, 100))
+            {
+                agent.destination = hit.point;
+            }
+        }
+    }
+}

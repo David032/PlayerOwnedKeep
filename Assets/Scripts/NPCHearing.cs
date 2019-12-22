c53a82265fc677786246482715f7ea7b04e1520c
David Kelly <davek032@outlook.com>
2019-12-22 18:14:05 +0000

    Audio Sense Implmented

---
+++ Assets/Scripts/NPCHearing.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(SphereCollider))]
+public class NPCHearing : Sense
+{
+    SphereCollider hearingRange;
+    protected override void Initialize()
+    {
+        hearingRange = GetComponent<SphereCollider>();
+    }
+    protected override void UpdateSense()
+    {
+        //elapsedTime += Time.deltaTime;
+        //if (elapsedTime >= detectionRate)
+        //{
+        //    listen();
+        //}
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+            
+    }
+}
+
+

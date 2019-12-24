7f8dff2e3a993500c7ddc7281f7a762b15a7851a
David Kelly <davek032@outlook.com>
2019-12-24 18:08:28 +0000

    Runtime event generation
    Have worked out how to do runtime event generation, but it's not the smoothest and can't tell if the NPC isn't in range, or can't see it

--- Assets/Scripts/EventManager.cs
+++ Assets/Scripts/EventManager.cs
@@ -6,15 +6,10 @@ public class EventManager : MonoBehaviour
 {
     public List<Event> Events;
 
-
     // Start is called before the first frame update
     void Start()
     {
-        //List<categories> testCategories = new List<categories>();
-        //testCategories.AddRange(new categories[] { categories.Good,categories.Dog});
-        //Event TestEvent = this.gameObject.AddComponent<Event>();
-        //TestEvent.CreateEvent("Test Event", testCategories, 0.75f, true);
-        //Events.Add(TestEvent);
+
     }
 
     // Update is called once per frame
---
+++ Assets/Scripts/EventTrigger.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EventTrigger : MonoBehaviour
+{
+    EventManager Manager;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Manager = GameObject.FindGameObjectWithTag("EventManager").GetComponent<EventManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            List<categories> testCategories = new List<categories>();
+            testCategories.AddRange(new categories[] { categories.Change });
+            Event TestEvent = Manager.gameObject.AddComponent<Event>();
+            TestEvent.CreateEvent("Someone new arrived", testCategories, 0.75f, true);
+            Manager.Events.Add(TestEvent);
+            Destroy(this.gameObject.GetComponent<BoxCollider>(), 5f);
+        }
+    }
+}
